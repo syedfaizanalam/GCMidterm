5edeb97dc173ccf131c4d0b1d27fdce5caf84f6a
Kavitha <kavithaa434@gmail.com>
2020-08-06 10:37:01 -0400

    second commit

--- src/CentralApp.java
+++ src/CentralApp.java
@@ -5,6 +5,7 @@ public class CentralApp {
 		System.out.println("Once upon a time in the mystical land of Shangrila there dwelt an immortal named Kai-Yun and his trusty band of ninja monkeys.");
 		System.out.println("They were very clever monkeys indeed, known for their ingenious teamwork.");
 		System.out.println("There was one monkey leader who led all the other monkeys.");
+		System.out.println("Once they were challenged by a swarm of seemingly unstoppable bugs. But following Kai-Yun’s unifying leadership, they paired up and mowed down their foes. ");
 	}
 
 }
