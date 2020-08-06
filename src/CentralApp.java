e6ad172372d56f641cb29d185774dfd343240c36
golamc2020 <golamc815@gmail.com>
2020-08-06 10:28:43 -0400

    lol

--- src/CentralApp.java
+++ src/CentralApp.java
@@ -3,7 +3,8 @@ public class CentralApp {
 
 	public static void main(String[] args) {
 		System.out.println("Once upon a time in the mystical land of Shangrila there dwelt an immortal named Kai-Yun and his trusty band of ninja monkeys.");
-
+		System.out.println("They were very clever monkeys indeed, known for their ingenious teamwork.");
+		
 	}
 
 }
