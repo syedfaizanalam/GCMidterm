857b27e01bc514abb17149fbf7b492dfdcd8472d
syedfaizanalam <43822557+syedfaizanalam@users.noreply.github.com>
2020-08-06 10:32:24 -0400

    update

--- src/CentralApp.java
+++ src/CentralApp.java
@@ -4,7 +4,7 @@ public class CentralApp {
 	public static void main(String[] args) {
 		System.out.println("Once upon a time in the mystical land of Shangrila there dwelt an immortal named Kai-Yun and his trusty band of ninja monkeys.");
 		System.out.println("They were very clever monkeys indeed, known for their ingenious teamwork.");
-		
+		System.out.println("There was one monkey leader who led all the other monkeys.");
 	}
 
 }
